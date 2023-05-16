2b599b0e71bff8adc56d9215b2f665e0e344abd8
Mihails Iljicevs <goenzi61@gmail.com>
2023-05-16 09:31:01 +0200

    Add cabinet models

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Employees, PlaceWork
+from .models import Employees, PlaceWork, Cabinet
 
 class AdminPlaceWork(admin.TabularInline):
     model = PlaceWork
@@ -8,3 +8,7 @@ class AdminPlaceWork(admin.TabularInline):
 class AdminEmpluyees(admin.ModelAdmin):
     inlines = [AdminPlaceWork,]
     list_display = ['last_name', 'first_name', 'surname']
+
+@admin.register(Cabinet)
+class AdminCabinet(admin.ModelAdmin):
+    list_display = ['number', 'floor', 'name', 'employee']
--- school/models.py
+++ school/models.py
@@ -27,5 +27,17 @@ class PlaceWork(models.Model):
     position = models.ManyToManyField("settings.Positions", verbose_name=_("Должность"))
     subject = models.ManyToManyField("settings.AcademicSubjects", verbose_name=_("Предметы:"))
 
+class Cabinet(models.Model):
+    school = models.ForeignKey("settings.Schools", verbose_name=_("Школа"), on_delete=models.CASCADE)
+    number = models.IntegerField(_("Номер кабинета"))
+    floor = models.IntegerField(_("Этаж (не обязательно)"), blank=True, null=True)
+    name = models.CharField(_("Название кабинета (не обязательно)"), max_length=100, blank=True, null=True)
+    employee = models.ForeignKey("school.Employees", verbose_name=_("Закрепленный педагог(не обязательно)"), on_delete=models.CASCADE, blank=True, null=True)
 
+    class Meta:
+        verbose_name = 'Кабинет'
+        verbose_name_plural = 'Кабинеты'
 
+    def __str__(self):
+        return f'{self.number} | {self.name}'
+    
