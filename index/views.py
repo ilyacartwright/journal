47fcc39fad4f90af9d6e3b121a9e1664321c9187
Mihails Iljicevs <goenzi61@gmail.com>
2023-05-16 08:51:46 +0200

    Displaying information about an employee

--- index/views.py
+++ index/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-import datetime
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from users.forms import AuthForm
 from django.contrib.auth.decorators import login_required
+import datetime
+
+from school.models import Employees
+from users.forms import AuthForm
 
 
 @login_required(login_url='index:login')
 def render_indexpage(request):
-        return render(request, 'page/auth/index.html', {})
+        employees = Employees.objects.get(user=request.user)
+        return render(request, 'page/auth/index.html', {'employees': employees,})
 
     
 class LoginUser(LoginView):
